7198ebc37524bcef793e823f621e397007d54bc0
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2019-11-15 14:15:40 +0200

    Create lesson5_hw05.py

---
+++ lesson5_hw05.py
@@ -0,0 +1,8 @@
+import random
+val1 = random.randint (100,999)
+val2 = random.randint (100,999)
+val3 = random.randint (100,999)
+l = (val1,val2,val3)
+print ("Your list is: ", l)
+print (sorted (l))
+доработать
