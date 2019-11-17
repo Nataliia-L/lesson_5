d3772dd304d394e66b2610bb65d338ce871d5f5a
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2019-11-17 16:45:39 +0200

    Update lesson5_hw05.py

--- lesson5_hw05.py
+++ lesson5_hw05.py
@@ -2,7 +2,6 @@ import random
 val1 = random.randint (100,999)
 val2 = random.randint (100,999)
 val3 = random.randint (100,999)
-l = (val1,val2,val3)
-print ("Your list is: ", l)
-print (sorted (l))
-доработать
+l = ([val1,val2,val3])
+print ("Your list is: ", sorted(l))
+print ("Your reversed list is: ", sorted(l, reverse = True))
