ffda668bb467b1f9b531d2b02f132f9c29fbc171
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2019-11-17 19:11:45 +0200

    Update lesson5_hw10.py

--- lesson5_hw10.py
+++ lesson5_hw10.py
@@ -1,6 +1,6 @@
 import random
 val = str (random.randint (0,9))
-val_30 = ("val"*30)
-print (val*30)
-print (val_30 (1000,3))
-add
+val2 = (list (val*30))
+print ("Current list: ", "".join(val2)) 
+val2[2] = str (1000)
+print ("New list: ", "".join(val2))
