dcf8a33f6516acc3e1da4ddf95fb47c17e6ecaab
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2019-11-20 16:07:24 +0200

    Update lesson5_hw06.py

--- lesson5_hw06.py
+++ lesson5_hw06.py
@@ -1,6 +1,7 @@
-t0 = dir()
+import string
+t0 = dir(string)
 t = t0 [-5:]
 print ("This is TUPLE: ", tuple (t))
 print ("This is LIST: ",list (t))
-t[2] = "capwords"
+t.insert (2,"capwords")
 print ("The result is: ", tuple (t))
