c90aa22b46d313bc4365cc9a95da06840812491d
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2019-11-20 16:09:21 +0200

    Create lesson 5.py

---
+++ lesson 5.py
@@ -0,0 +1,63 @@
+#Создание списков, присвоение значений
+#  l = [1, "hello_me", 23.58, True]
+# print (l)
+# a = "hello"
+# val1 = [a, 52]
+# print (val1)
+# val2 = [52, a]
+# print ([val1] == [val2])
+# print (val1 [0])
+# Ниже срез и присваивание новых значений
+# l = [1, 2, 3, 4]
+# l[0] = "T"
+# print (l)
+# print (l [1:-1])
+# l[1:-1] = [5,6,7,8]
+# print (l)
+# l[1:4] = ["Test"]
+# print (l)
+# Вложенность
+# l = [1 ,2,["D","f"]]
+# print (l)
+# print (l[2][1])
+# l[2][1] = "Oops"
+# print (l)
+# Копирование
+a = [1,2,5,"G",[22.5,22.6,33.5]]
+# b = a
+# a[0] = "U"
+# print (a)
+# print (b)
+# b = a[:]
+# print (b)
+# b = list(a)
+# print (b)
+# import copy
+# b = copy.deepcopy(a)
+# print ('G' in a)
+# print (22.8 in b)
+# print ('G' not in a)
+# print (a*5)
+# print (len(a))
+# print (dir(a))
+# Методы списков
+# print (a.append(1))
+# print (a)
+# print (a.insert(2,5))
+# print (a)
+# d = [1,2,2,2,5,6,8]
+# # print (d.remove(2))
+# print (d)
+# # print (d.index(9))
+# print (d.count (2))
+# print (help (d.sort))
+# Tuple
+t = (1,2,"Hello", True)
+print (t)
+print (t[:2])
+print (t[::-1])
+# t[0] = "New"
+t = 1,
+print (type(t))
+t = (1)
+print (type(t))
